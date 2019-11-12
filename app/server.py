8ababe31ed07fe0da97173d91ad86f50ef8359fd
CO16343_Rajanshu <35761455+CO16343@users.noreply.github.com>
2019-11-12 22:48:51 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -12,6 +12,16 @@ from starlette.staticfiles import StaticFiles
 from char_seg import *
 
 
+import os
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/path/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+app_new = Flask(__name__)
+app_new.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 export_file_url = 'https://www.googleapis.com/drive/v3/files/1-4Bszwv0HDTmHd2LEQmDl3KfMP0z9PuC?alt=media&key=AIzaSyBg-HoeVUlHZNf5YgAtPmhDFNrVnAD-WuQ'
 export_file_name = 'export.pkl'
 
@@ -21,6 +31,29 @@ path = Path(__file__).parent
 app = Starlette()
 app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
 app.mount('/static', StaticFiles(directory='app/static'))
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
 
 
 async def download_file(url, dest):
