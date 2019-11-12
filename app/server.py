52bb503e6400d7c6e3c118a70b988bac603ede67
CO16343_Rajanshu <35761455+CO16343@users.noreply.github.com>
2019-11-12 22:32:26 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -194,7 +194,7 @@ async def analyze(request):
 #    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     #cv2.imwrite('new.jpg',gray)
     #img2 = cv2.imread(img,1) 
-    img2 = cv2.resize(img, dsize =(1320, int(1320*height/width)), interpolation = cv2.INTER_AREA)#lines, lines_img, x_lines = start_main(img2)
+    img2 = cv2.resize(img, dsize =(1320, int(1320*250/500)), interpolation = cv2.INTER_AREA)#lines, lines_img, x_lines = start_main(img2)
     final_out = ''
 #    for i in range(len(lines)):    # i is the line number
 #	    final_out = final_out + letter_seg(lines_img, x_lines, i)	#all
