b67bc0835b0699e54f42b144a265fdd65c0db4a3
CO16343_Rajanshu <35761455+CO16343@users.noreply.github.com>
2019-11-14 00:12:04 +0530

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -30,7 +30,7 @@ function analyze() {
   xhr.onload = function(e) {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
-      el("result-label").innerHTML = `Result = ${response["result"]}`;
+      el("result-label").innerHTML = `${response["result"]}`;
       el("process-label").innerHTML = ``;
     }
     el("analyze-button").innerHTML = "Analyze";
